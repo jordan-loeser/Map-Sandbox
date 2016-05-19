744a74c5f0f931d589ce1826fc67894d174521af
Jordan Loeser <jordlo@comcast.net>
2016-05-19 11:38:55 -0400

    Functioning Map with Tilt Toggle

--- js/maps.js
+++ js/maps.js
@@ -1,14 +1,16 @@
+var centerCoord = {lat: 39.7684, lng: -86.1581};
+
 function initMap() {
   var mapDiv = document.getElementById('map');
-  var map = new google.maps.Map(mapDiv, {
-    center: {lat: 40.506977, lng: -73.886787},
-    tilt: 0,
-    mapTypeId: google.maps.MapTypeId.HYBRID,
-    zoom: 10,
+  var mapOptions = {
+    center: centerCoord, //{lat: 40.506977, lng: -73.886787},
+    mapTypeId: google.maps.MapTypeId.SATELLITE,
+    zoom: 18,
+    tilt: 45,
     draggable: false,
-    scrollwheel: false,
+    //scrollwheel: false,
     disableDoubleClickZoom: true,
-    disableDefaultUI: true
+    disableDefaultUI: true,
       //mapTypeControl: true,
       //panControl: true,
       //rotateControl: true,
@@ -16,5 +18,20 @@ function initMap() {
       //scaleControl: true,
       //zoomControl: true,
       //scrollwheel: true,
+  };
+
+  var map = new google.maps.Map(mapDiv, mapOptions);
+
+  google.maps.event.addListener(map, 'tilt_changed', function() {
+    // Add perspective if google maps can't do it for us
+    if (map.getTilt() == 0) {
+      mapDiv.className = "hasPerspective";
+    } else { // Remove perspective when google maps can do it for us
+      mapDiv.className = "";
+      map.setHeading(45);
+    }
+    google.maps.event.trigger(map, "resize");
+    map.setCenter(centerCoord);
   });
+
 }
