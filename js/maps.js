624312c693a14479e12dafb9de321bd1045fa547
Jordan Loeser <jordlo@comcast.net>
2016-05-23 12:40:24 -0400

    Added Circle Marker

--- js/maps.js
+++ js/maps.js
@@ -1,14 +1,14 @@
-var centerCoord = {lat: 39.7684, lng: -86.1581}; //{lat: 40.506977, lng: -73.886787},
+var centerCoord = {lat: 25.796645, lng: -80.126005}; // {lat: 39.7684, lng: -86.1581}; //{lat: 40.506977, lng: -73.886787},
 
 function initMap() {
   var mapDiv = document.getElementById('map');
   var mapOptions = {
     center: centerCoord,
     mapTypeId: google.maps.MapTypeId.SATELLITE,
-    zoom: 11,
+    zoom: 18, // 11
     tilt: 45,
     draggable: false,
-    scrollwheel: false,
+    //scrollwheel: false,
     disableDoubleClickZoom: true,
     disableDefaultUI: true,
       //mapTypeControl: true,
@@ -24,6 +24,36 @@ function initMap() {
   google.maps.event.trigger(map, "resize");
   map.setCenter(centerCoord); // Recenter when resized
 
+
+  // Initialize Sizes
+  var zoom = map.getZoom();
+  var cloud = document.getElementsByClassName('cloud');
+  var orbit = document.getElementsByClassName('orbit');
+  for(var i = 0; i < cloud.length; i++) {
+    cloud[i].style.width = (zoom / 22) * 245 + "px";
+  }
+  for(var i = 0; i < orbit.length; i++) {
+    if(zoom < 10) {
+      orbit[i].src = "images/satellite.png";
+      orbit[i].style.width = (zoom / 22) * 500 + "px";
+    } else {
+      orbit[i].src = "images/plane.png";
+      orbit[i].style.width = (zoom / 22) * 100 + "px";
+    }
+  }
+
+  // Add Marker
+  var marker = new google.maps.Marker({
+    position: centerCoord,
+    animation: google.maps.Animation.DROP,
+    map: map,
+    icon: {
+    path: google.maps.SymbolPath.CIRCLE,
+    scale: 10
+  },
+    title: 'Hello World!'
+  });
+
   // Adjust Tilt
   google.maps.event.addListener(map, 'tilt_changed', function() {
     // Add perspective if google maps can't do it for us
@@ -31,7 +61,7 @@ function initMap() {
       mapDiv.className = "hasPerspective";
     } else { // Remove perspective when google maps can do it for us
       mapDiv.className = "flat";
-      map.setHeading(45);
+      map.setHeading(0);
     }
     google.maps.event.trigger(map, "resize");
     map.setCenter(centerCoord); // Recenter when resized
@@ -41,8 +71,18 @@ function initMap() {
   google.maps.event.addListener(map, 'zoom_changed', function() {
     var zoom = map.getZoom();
     var cloud = document.getElementsByClassName('cloud');
+    var orbit = document.getElementsByClassName('orbit');
     for(var i = 0; i < cloud.length; i++) {
       cloud[i].style.width = (zoom / 22) * 245 + "px";
+    }
+    for(var i = 0; i < orbit.length; i++) {
+      if(zoom < 10) {
+        orbit[i].src = "images/satellite.png";
+        orbit[i].style.width = (zoom / 22) * 500 + "px";
+      } else {
+        orbit[i].src = "images/plane.png";
+        orbit[i].style.width = (zoom / 22) * 100 + "px";
+      }
     }
     google.maps.event.trigger(map, "resize");
     map.setCenter(centerCoord); // Recenter when resized
